276d37b9b4b5e454a72aaae9c17a68612003dcf6
MusicalProf <krishon2006@gmail.com>
2021-10-26 05:03:08 -0400

    Nothnig to update. Commented out certain portions of code.

--- src/labs/Lab1.java
+++ src/labs/Lab1.java
@@ -1,26 +1,25 @@
 package labs;
 
-import java.util.Arrays;
 
 public class Lab1 {
   public static void main(String[] args){
     // 1. Write a function that takes a value n and returns the sum of numbers 1 to n.
-    System.out.println(sumNumbers(10)); 
+    // System.out.println(sumNumbers(10)); 
 
     // 2. Write a function that computes the factorial value
-    // int i = 0;
-    // while(i <= 15){
-    //   System.out.println(factorial(i));
-    //   i++;
-    // }
+    int i = 0;
+    while(i <= 15){
+      System.out.println(factorial(i));
+      i++;
+    }
     System.out.println(factorial(10));
 
     // 3. Write 3 functions that take an array as a parameter and return the minimum, average, and maximum values of that array.
-    int[] arr = new int[] {4, 6, 10, 5, 20, 2, 13, 3, 8, 16};
-    System.out.println(Arrays.toString(arr));
-    System.out.println("The max is: " + max(arr));
-    System.out.println("The min is: " + min(arr));
-    System.out.println("The average is: " + avg(arr));
+    // int[] arr = new int[] {4, 6, 10, 5, 20, 2, 13, 3, 8, 16};
+    // System.out.println(Arrays.toString(arr));
+    // System.out.println("The max is: " + max(arr));
+    // System.out.println("The min is: " + min(arr));
+    // System.out.println("The average is: " + avg(arr));
     
   } 
 
